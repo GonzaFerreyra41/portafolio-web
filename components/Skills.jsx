239eaf1fbaee78c333c31f68cea0c6195b7cc17e
GonzaFerreyra.21 <gonzalo41092@gmail.com>
2022-10-26 19:22:06 -0300

    Icons img

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -15,7 +15,7 @@ const Skills = () => {
             <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
                 <div className="grid grid-cols-2 gap-4 justify-center items-center"> 
                     <div className="m-auto">
-                        <Image src="/../public/assets/skills/html.png" 
+                        <Image src="/../public/assets/img/html.png" 
                             width="64px"
                             height="64px"
                             alt="Icons" />
