a10f8c981e156ee15c038f76502df94082dff446
GonzaFerreyra.21 <gonzalo41092@gmail.com>
2022-12-26 19:48:29 -0300

    tamaño de las fuentes

--- components/Main.jsx
+++ components/Main.jsx
@@ -3,7 +3,7 @@ import React from "react"
 
 const Main = () => {
     return (
-    <div id='home' className="w-full h-full sm:md:pb-[1.5rem] text-center">
+    <div id='home' className="w-full h-screen sm:md:pb-[1.5rem] text-center">
         <div className="max-w-[1240px] px-2 mx-auto flex justify-center items-center ">
             <div className="pt-[9rem] pb-[9rem] ">
                 <p className="uppercase tracking-widest text-[18px] pb-2 text-gray-500">
@@ -12,7 +12,7 @@ const Main = () => {
                 <h1 className=" text-[#000]">
                     Hola, soy <span className="text-[#5651e5]">Gonzalo</span>  Desarrollador Web Front-End Developer 
                 </h1>
-                <p className="py-4 text-[18px] sm:md:text-[28px]  text-gray-500 sm:max-w-[70%] m-auto">
+                <p className="py-4 text-[20px] sm:md:text-[28px]  text-gray-500 sm:max-w-[70%] m-auto">
                     Experiencia en la creación de sitios web y aplicaciones web. Me especializo en JavaScript y tengo experiencia con React.
                 </p>
             </div>
